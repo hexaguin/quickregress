92babdd966f42db7f87cc6b1af5c45f9f6f1e1d1
Ryan Hattie <hexaguin@gmail.com>
2020-01-23 22:04:15 -0700

    One more time, with feeling!

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='quickregress',         # How you named your package folder (MyLib)
-    packages=['quickregress'],   # Chose the same as "name"
-    version='0.1.2',      # Start with a small number and increase it with every change you make
-    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description='Provides a low-effort wrapper for sklearn\'s polynomial regression',   # Give a short description about your library
+    name='quickregress',
+    packages=['quickregress'],
+    version='0.1.2',
+    license='MIT',
+    description='Provides a low-effort wrapper for sklearn\'s polynomial regression',
     long_description_content_type="text/markdown",
     long_description=long_description,
-    author='Ryan Hattie',                   # Type in your name
-    author_email='ryan@hattie.email',      # Type in your E-Mail
-    url='https://github.com/hexaguin/quickregress',   # Provide either the link to your github or to your website
-    download_url='https://github.com/hexaguin/quickregress/archive/v_01.tar.gz',    # I explain this later on
+    author='Ryan Hattie',
+    author_email='ryan@hattie.email',
+    url='https://github.com/hexaguin/quickregress',
     keywords=['sklearn', 'regression'],
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'numpy',
         'sklearn',
     ],
